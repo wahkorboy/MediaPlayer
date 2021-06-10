323aaca6a1e4685f3f8ea00acc53abe3523837f4
iryu <namboonma@gmail.com>
2021-06-10 11:11:59 +0700

    todo next fix delete button

--- app/src/main/java/com/wahkor/mediaplayer/receiver/AudioReceiver.kt
+++ app/src/main/java/com/wahkor/mediaplayer/receiver/AudioReceiver.kt
@@ -1,8 +1,10 @@
 package com.wahkor.mediaplayer.receiver
 
+import BackgroundMediaService
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.session.MediaSession
 import android.util.Log
 import android.widget.Toast
 import com.wahkor.mediaplayer.MusicPlayer
@@ -14,6 +16,10 @@ class AudioReceiver:BroadcastReceiver() {
         if (intent?.action.equals(Intent.ACTION_MEDIA_BUTTON)) {
 
         }
+
+    }
+    fun handleIntent(mediaSession:MediaSession?, intent:Intent?){
+
     }
 
 
