1d3679cfb044dd35467468ecfb7e8450570569d0
namboonma <namboonma@gmail.com>
2021-06-13 10:45:05 +0700

    Add AudioFocus ,AudioBecoming Noisy

--- app/src/main/java/com/wahkor/mediaplayer/MusicPlayerActivity.kt
+++ app/src/main/java/com/wahkor/mediaplayer/MusicPlayerActivity.kt
@@ -15,46 +15,50 @@ import com.wahkor.mediaplayer.adapter.PlaylistAdapter
 import com.wahkor.mediaplayer.model.Song
 import com.wahkor.mediaplayer.service.AudioService
 
-class MusicPlayerActivity : AppCompatActivity(),MenuInterface {
+class MusicPlayerActivity : AppCompatActivity(), MenuInterface {
     private lateinit var menuImageView: ImageView
     private lateinit var settingImageView: ImageView
     private lateinit var titleView: TextView
     private lateinit var seekBar: SeekBar
     private lateinit var prevBTN: ImageView
-    private lateinit var playBTN:ImageView
-    private lateinit var nextBTN:ImageView
-    private lateinit var recyclerView:RecyclerView
-    private lateinit var adapter:PlaylistAdapter
-    private lateinit var playlistName:TextView
-    private var songQuery=ArrayList<Song>()
-    private var handler=Handler(Looper.getMainLooper())
+    private lateinit var playBTN: ImageView
+    private lateinit var nextBTN: ImageView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: PlaylistAdapter
+    private lateinit var playlistName: TextView
+    private lateinit var tvPass: TextView
+    private lateinit var tvDue: TextView
+    private var songQuery = ArrayList<Song>()
+    private var handler = Handler(Looper.getMainLooper())
     private lateinit var runnable: Runnable
-    private var mp=AudioService()
+    private var mp = AudioService()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_player)
-        songQuery=mp.getSongQuery
-        menuImageView=findViewById(R.id.music_player_menu)
-        settingImageView=findViewById(R.id.music_player_setting)
-        titleView=findViewById(R.id.music_player_Title)
-        seekBar=findViewById(R.id.music_player_Seekbar)
-        prevBTN=findViewById(R.id.music_player_Prev)
-        playBTN=findViewById(R.id.music_player_Play)
-        nextBTN=findViewById(R.id.music_player_Next)
-        playlistName=findViewById(R.id.music_player_playlistName)
-        recyclerView=findViewById(R.id.music_player_ListView)
+        songQuery = mp.getSongQuery
+        menuImageView = findViewById(R.id.music_player_menu)
+        settingImageView = findViewById(R.id.music_player_setting)
+        titleView = findViewById(R.id.music_player_Title)
+        seekBar = findViewById(R.id.music_player_Seekbar)
+        prevBTN = findViewById(R.id.music_player_Prev)
+        playBTN = findViewById(R.id.music_player_Play)
+        nextBTN = findViewById(R.id.music_player_Next)
+        playlistName = findViewById(R.id.music_player_playlistName)
+        recyclerView = findViewById(R.id.music_player_ListView)
+        tvPass = findViewById(R.id.music_player_tv_pass)
+        tvDue = findViewById(R.id.music_player_tv_due)
         prevBTN.setOnClickListener { mp.prevPlay() }
         playBTN.setOnClickListener {
-            if(mp.isPlaying()){
+            if (mp.isPlaying()) {
                 mp.pause()
-            }else{
+            } else {
                 mp.start()
             }
         }
         nextBTN.setOnClickListener { mp.nextPlay() }
-        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if(fromUser){
+                if (fromUser) {
                     mp.seekTo(progress)
                 }
             }
@@ -67,8 +71,8 @@ class MusicPlayerActivity : AppCompatActivity(),MenuInterface {
 
         })
         menuImageView.setOnClickListener {
-            setOnMenuClick(this, PopupMenu(this,it as ImageView),mp.getTableName!!){intent ->
-            startActivity(intent)
+            setOnMenuClick(this, PopupMenu(this, it as ImageView), mp.getTableName!!) { intent ->
+                startActivity(intent)
             }
         }
         settingImageView.setOnClickListener {
@@ -76,17 +80,17 @@ class MusicPlayerActivity : AppCompatActivity(),MenuInterface {
                 startActivity(intent)
             }
         }
-        adapter= PlaylistAdapter(songQuery){ newList, action ->
-            when(action){
-                "ItemClicked"->if(mp.playlistAction(newList)){
+        adapter = PlaylistAdapter(songQuery) { newList, action ->
+            when (action) {
+                "ItemClicked" -> if (mp.playlistAction(newList)) {
                     mp.start()
                 }
-                "ItemMoved"->mp.playlistMoved(newList)
-                "ItemRemoved"->mp.playlistRemoved(newList)
+                "ItemMoved" -> mp.playlistMoved(newList)
+                "ItemRemoved" -> mp.playlistRemoved(newList)
             }
         }
-        recyclerView.layoutManager=LinearLayoutManager(this)
-        recyclerView.adapter=adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
 
         val callback = CustomItemTouchHelperCallback(adapter)
         val itemTouchHelper = ItemTouchHelper(callback)
@@ -96,32 +100,35 @@ class MusicPlayerActivity : AppCompatActivity(),MenuInterface {
         adapter.notifyDataSetChanged()
     }
 
-    private fun setRunnable(){
-        runnable= Runnable {
+    private fun setRunnable() {
+        runnable = Runnable {
             mp.getTableName?.let {
-                playlistName.text=it
+                playlistName.text = it
             }
-            if(mp.isUpdate){
-                songQuery=mp.getSongQuery
+            if (mp.isUpdate) {
+                songQuery = mp.getSongQuery
                 adapter.notifyDataSetChanged()
                 mp.clearUpdate()
-                for(i in 0 until songQuery.size){
-                    if(songQuery[i].is_playing)
+                for (i in 0 until songQuery.size) {
+                    if (songQuery[i].is_playing)
                         recyclerView.scrollToPosition(i)
                 }
             }
-                titleView.text=mp.title
-                seekBar.max=mp.duration
-                seekBar.progress=mp.currentPosition
-                playBTN.setImageDrawable(
-                    if (mp.isPlaying())
-                        ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_pause, null)
-                    else
-                        ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_play, null))
+            titleView.text = mp.title
+            seekBar.max = mp.duration
+            seekBar.progress = mp.currentPosition
+            tvPass.text = getMinute(mp.currentPosition)
+            tvDue.text = getMinute(mp.duration-mp.currentPosition)
+            playBTN.setImageDrawable(
+                if (mp.isPlaying())
+                    ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_pause, null)
+                else
+                    ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_play, null)
+            )
 
-            handler.postDelayed(runnable,1000)
+            handler.postDelayed(runnable, 1000)
         }
-        handler.postDelayed(runnable,1000)
+        handler.postDelayed(runnable, 1000)
     }
 
 }
